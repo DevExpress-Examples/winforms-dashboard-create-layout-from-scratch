10fdf3f9e1925b981ebb04342659030b5096a1f7
DevExpressExampleBot <fakeEmail>
2018-04-24 19:06:49 +0300

    Source auto update [skip ci]

--- VB/Dashboard_LayoutCustomization/Form1.vb
+++ VB/Dashboard_LayoutCustomization/Form1.vb
@@ -1,18 +1,17 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports DevExpress.XtraEditors
 Imports DevExpress.DashboardCommon
 
 Namespace Dashboard_LayoutCustomization
-	Partial Public Class Form1
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
-			dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
-		End Sub
+    Partial Public Class Form1
+        Inherits XtraForm
 
-        Private Sub btnUpdateLayout_Click(ByVal sender As Object, ByVal e As EventArgs) _
-                Handles btnUpdateLayout.Click
+        Public Sub New()
+            InitializeComponent()
+            dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
+        End Sub
+
+        Private Sub btnUpdateLayout_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnUpdateLayout.Click
             Dim dashboard As New Dashboard()
             dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
             Dim grid As DashboardItem = CType(dashboard.Items(0), GridDashboardItem)
@@ -25,17 +24,15 @@ Namespace Dashboard_LayoutCustomization
             Dim rangeItem As New DashboardLayoutItem(range, 20)
 
             ' Creates a layout group that contains two layout items.
-            Dim group1 As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, _
-                                                   80, gridItem, chartItem)
+            Dim group1 As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, 80, gridItem, chartItem)
             ' Creates a root layout group that contains the previously created group and 
             ' the layout item displaying the Range Filter dashboard item. 
-            Dim rootGroup As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, _
-                                                    1, group1, rangeItem)
+            Dim rootGroup As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, 1, group1, rangeItem)
 
             ' Specifies the root dashboard layout group.
             dashboard.LayoutRoot = rootGroup
 
             dashboardViewer1.Dashboard = dashboard
         End Sub
-	End Class
+    End Class
 End Namespace
--- VB/Dashboard_LayoutCustomization/Program.vb
+++ VB/Dashboard_LayoutCustomization/Program.vb
@@ -1,21 +1,22 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Windows.Forms
 Imports DevExpress.Skins
 
 Namespace Dashboard_LayoutCustomization
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			SkinManager.EnableFormSkins()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Form1())
-		End Sub
-	End Class
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread> _
+        Shared Sub Main()
+            SkinManager.EnableFormSkins()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Application.Run(New Form1())
+        End Sub
+    End Class
 End Namespace
