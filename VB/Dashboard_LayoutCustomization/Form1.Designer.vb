27a3c71b51f2bf45e0b9ee117aea7c29527e870e
DevExpressExampleBot <fakeEmail>
2023-10-11 19:25:36 +0400

    Source auto update [skip ci]

--- VB/Dashboard_LayoutCustomization/Form1.Designer.vb
+++ VB/Dashboard_LayoutCustomization/Form1.Designer.vb
@@ -1,73 +1,71 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace Dashboard_LayoutCustomization
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			Me.btnUpdateLayout = New System.Windows.Forms.Button()
-			CType(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardViewer1
-			' 
-			Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer1.Name = "dashboardViewer1"
-			Me.dashboardViewer1.PrintingOptions.DocumentContentOptions.FilterState = DevExpress.DashboardWin.DashboardPrintingFilterState.None
-			Me.dashboardViewer1.Size = New System.Drawing.Size(890, 459)
-			Me.dashboardViewer1.TabIndex = 0
-			' 
-			' btnUpdateLayout
-			' 
-			Me.btnUpdateLayout.Location = New System.Drawing.Point(0, 0)
-			Me.btnUpdateLayout.Name = "btnUpdateLayout"
-			Me.btnUpdateLayout.Size = New System.Drawing.Size(105, 23)
-			Me.btnUpdateLayout.TabIndex = 1
-			Me.btnUpdateLayout.Text = "Update Layout"
-			Me.btnUpdateLayout.UseVisualStyleBackColor = True
-'			Me.btnUpdateLayout.Click += New System.EventHandler(Me.btnUpdateLayout_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(890, 459)
-			Me.Controls.Add(Me.btnUpdateLayout)
-			Me.Controls.Add(Me.dashboardViewer1)
-			Me.Name = "Form1"
-			Me.Text = "Dashboard Viewer"
-			CType(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            Me.btnUpdateLayout = New System.Windows.Forms.Button()
+            CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardViewer1
+            ' 
+            Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer1.Name = "dashboardViewer1"
+            Me.dashboardViewer1.PrintingOptions.DocumentContentOptions.FilterState = DevExpress.DashboardWin.DashboardPrintingFilterState.None
+            Me.dashboardViewer1.Size = New System.Drawing.Size(890, 459)
+            Me.dashboardViewer1.TabIndex = 0
+            ' 
+            ' btnUpdateLayout
+            ' 
+            Me.btnUpdateLayout.Location = New System.Drawing.Point(0, 0)
+            Me.btnUpdateLayout.Name = "btnUpdateLayout"
+            Me.btnUpdateLayout.Size = New System.Drawing.Size(105, 23)
+            Me.btnUpdateLayout.TabIndex = 1
+            Me.btnUpdateLayout.Text = "Update Layout"
+            Me.btnUpdateLayout.UseVisualStyleBackColor = True
+            AddHandler Me.btnUpdateLayout.Click, New System.EventHandler(AddressOf Me.btnUpdateLayout_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(890, 459)
+            Me.Controls.Add(Me.btnUpdateLayout)
+            Me.Controls.Add(Me.dashboardViewer1)
+            Me.Name = "Form1"
+            Me.Text = "Dashboard Viewer"
+            CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
-		Private WithEvents btnUpdateLayout As System.Windows.Forms.Button
-	End Class
+'#End Region
+        Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
+
+        Private btnUpdateLayout As System.Windows.Forms.Button
+    End Class
 End Namespace
-
--- VB/Dashboard_LayoutCustomization/Form1.vb
+++ VB/Dashboard_LayoutCustomization/Form1.vb
@@ -1,41 +1,35 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports DevExpress.XtraEditors
 Imports DevExpress.DashboardCommon
 
 Namespace Dashboard_LayoutCustomization
-	Partial Public Class Form1
-		Inherits XtraForm
-		Public Sub New()
-			InitializeComponent()
-			dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
-		End Sub
 
-        Private Sub btnUpdateLayout_Click(ByVal sender As Object, ByVal e As EventArgs) _
-                Handles btnUpdateLayout.Click
-            Dim dashboard As New Dashboard()
+    Public Partial Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            dashboardViewer1.LoadDashboard("..\..\Data\Dashboard.xml")
+        End Sub
+
+        Private Sub btnUpdateLayout_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim dashboard As Dashboard = New Dashboard()
             dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
             Dim grid As DashboardItem = CType(dashboard.Items(0), GridDashboardItem)
             Dim chart As DashboardItem = CType(dashboard.Items(1), ChartDashboardItem)
             Dim range As DashboardItem = CType(dashboard.Items(2), RangeFilterDashboardItem)
-
             ' Creates layout items used to display dashboard items.
-            Dim gridItem As New DashboardLayoutItem(grid, 35)
-            Dim chartItem As New DashboardLayoutItem(chart, 65)
-            Dim rangeItem As New DashboardLayoutItem(range, 20)
-
+            Dim gridItem As DashboardLayoutItem = New DashboardLayoutItem(grid, 35)
+            Dim chartItem As DashboardLayoutItem = New DashboardLayoutItem(chart, 65)
+            Dim rangeItem As DashboardLayoutItem = New DashboardLayoutItem(range, 20)
             ' Creates a layout group that contains two layout items.
-            Dim group1 As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, _
-                                                   80, gridItem, chartItem)
+            Dim group1 As DashboardLayoutGroup = New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, 80, gridItem, chartItem)
             ' Creates a root layout group that contains the previously created group and 
             ' the layout item displaying the Range Filter dashboard item. 
-            Dim rootGroup As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, _
-                                                    1, group1, rangeItem)
-
+            Dim rootGroup As DashboardLayoutGroup = New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, 1, group1, rangeItem)
             ' Specifies the root dashboard layout group.
             dashboard.LayoutRoot = rootGroup
-
             dashboardViewer1.Dashboard = dashboard
         End Sub
-	End Class
+    End Class
 End Namespace
