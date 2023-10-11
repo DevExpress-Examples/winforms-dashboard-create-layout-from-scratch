70c9751396f190533b8b27bbfc1a62b43810a5da
DevExpressExampleBot <fakeEmail>
2023-10-11 19:24:37 +0400

    Source auto update [skip ci]

--- VB/Dashboard_LayoutCustomization/Form1.vb
+++ VB/Dashboard_LayoutCustomization/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Linq
 Imports DevExpress.XtraEditors
 Imports DevExpress.DashboardCommon
@@ -6,7 +6,8 @@ Imports DevExpress.DashboardWin
 Imports System.IO
 
 Namespace Dashboard_LayoutCustomization
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits XtraForm
 
         Public Sub New()
@@ -16,22 +17,20 @@ Namespace Dashboard_LayoutCustomization
         End Sub
 
         Private Sub ChangeLayout(ByVal args As DashboardToolbarItemClickEventArgs)
-            Dim dashboard As New Dashboard()
+            Dim dashboard As Dashboard = New Dashboard()
             dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
             dashboard.BeginUpdate()
-
             ' Create layout items used to display dashboard items.
-            Dim item0 As New DashboardLayoutItem(dashboard.Items(0), 35)
-            Dim item1 As New DashboardLayoutItem(dashboard.Items(1), 65)
-            Dim item2 As New DashboardLayoutItem(dashboard.Items(2), 20)
+            Dim item0 As DashboardLayoutItem = New DashboardLayoutItem(dashboard.Items(0), 35)
+            Dim item1 As DashboardLayoutItem = New DashboardLayoutItem(dashboard.Items(1), 65)
+            Dim item2 As DashboardLayoutItem = New DashboardLayoutItem(dashboard.Items(2), 20)
             ' Create a layout group that contains two layout items.
-            Dim group1 As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, 80, item0, item1)
+            Dim group1 As DashboardLayoutGroup = New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Horizontal, 80, item0, item1)
             ' Create a root layout group that contains the previously created group and 
             ' another layout item. 
-            Dim rootGroup As New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, 1, group1, item2)
+            Dim rootGroup As DashboardLayoutGroup = New DashboardLayoutGroup(DashboardLayoutGroupOrientation.Vertical, 1, group1, item2)
             ' Applies the layout to the dashboard.
             dashboard.LayoutRoot = rootGroup
-
             dashboard.EndUpdate()
             dashboardViewer1.Dashboard = dashboard
         End Sub
@@ -39,9 +38,7 @@ Namespace Dashboard_LayoutCustomization
         Private Sub RepositionItems(ByVal args As DashboardToolbarItemClickEventArgs)
             Dim dashboard As Dashboard = dashboardViewer1.Dashboard
             Dim rootGroup As DashboardLayoutGroup = TryCast(dashboard.LayoutRoot.ChildNodes(0), DashboardLayoutGroup)
-            If (rootGroup IsNot Nothing) AndAlso (rootGroup.ChildNodes.Count > 1) Then
-                rootGroup.ChildNodes(0).MoveRight(rootGroup.ChildNodes(1))
-            End If
+            If rootGroup IsNot Nothing AndAlso rootGroup.ChildNodes.Count > 1 Then rootGroup.ChildNodes(0).MoveRight(rootGroup.ChildNodes(1))
         End Sub
 
         Private Sub SaveLayoutToFile(ByVal args As DashboardToolbarItemClickEventArgs)
@@ -49,31 +46,27 @@ Namespace Dashboard_LayoutCustomization
         End Sub
 
         Private Sub LoadLayoutFromFile(ByVal obj As DashboardToolbarItemClickEventArgs)
-            If File.Exists("DashboardLayout.xml") Then
-                dashboardViewer1.LoadDashboardLayout("DashboardLayout.xml")
-            End If
+            If File.Exists("DashboardLayout.xml") Then dashboardViewer1.LoadDashboardLayout("DashboardLayout.xml")
         End Sub
 
         Private Sub DashboardViewer1_CustomizeDashboardTitle(ByVal sender As Object, ByVal e As CustomizeDashboardTitleEventArgs)
             Dim exportItem = e.Items.FirstOrDefault(Function(i) i.ButtonType = DashboardButtonType.Export)
-            If exportItem IsNot Nothing Then
-                e.Items.Remove(exportItem)
-            End If
-            Dim btnChangeLayout As New DashboardToolbarItem()
+            If exportItem IsNot Nothing Then e.Items.Remove(exportItem)
+            Dim btnChangeLayout As DashboardToolbarItem = New DashboardToolbarItem()
             btnChangeLayout.Caption = "Change Layout"
-            btnChangeLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf ChangeLayout)
+            btnChangeLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf Me.ChangeLayout)
             e.Items.Add(btnChangeLayout)
-            Dim btnRepositionItems As New DashboardToolbarItem()
+            Dim btnRepositionItems As DashboardToolbarItem = New DashboardToolbarItem()
             btnRepositionItems.Caption = "Reposition Items"
             btnRepositionItems.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf RepositionItems)
             e.Items.Add(btnRepositionItems)
-            Dim btnSaveLayout As New DashboardToolbarItem()
+            Dim btnSaveLayout As DashboardToolbarItem = New DashboardToolbarItem()
             btnSaveLayout.Caption = "Save Layout to File"
-            btnSaveLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf SaveLayoutToFile)
+            btnSaveLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf Me.SaveLayoutToFile)
             e.Items.Add(btnSaveLayout)
-            Dim btnLoadLayout As New DashboardToolbarItem()
+            Dim btnLoadLayout As DashboardToolbarItem = New DashboardToolbarItem()
             btnLoadLayout.Caption = "Load Layout from File"
-            btnLoadLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf LoadLayoutFromFile)
+            btnLoadLayout.ClickAction = New Action(Of DashboardToolbarItemClickEventArgs)(AddressOf Me.LoadLayoutFromFile)
             e.Items.Add(btnLoadLayout)
         End Sub
     End Class
